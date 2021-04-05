e0a8dc032441e13347aa8136c57923c5d543711b
Muhammadhassan63 <80644042+Muhammadhassan63@users.noreply.github.com>
2021-04-05 22:35:55 +0500

    Add files via upload

---
+++ Lab6/lab6_activity3_runner/ABC.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab6_activity3_runner;
+
+
+class ABC {
+    public static int i ;
+    public String s;
+
+    public static void displayStatic() 
+    {
+        System.out.println("i:"+i);
+    }
+    public void display() 
+    {
+        System.out.println("i:  "+i);
+        System.out.println("s:  "+s);
+    }
+}
+    
+
---
+++ Lab6/lab6_activity3_runner/Lab6_Activity3_Runner.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab6_activity3_runner;
+
+/**
+ *
+ * @author abc
+ */
+public class Lab6_Activity3_Runner {
+
+    
+    public static void main(String[] args) {
+        ABC a = new ABC();
+        a.display();
+        ABC.displayStatic();
+    }
+}
+ 
